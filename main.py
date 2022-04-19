a5eda9458363da42ddfd9c4405e3d216d53da7c9
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2022-04-19 17:29:46 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, Pycom Limited.
+
+from network import WLAN
+from mqtt import MQTTClient
+import machine
+import time
+
+def settimeout(duration): 
+    pass
+
+wlan = WLAN(mode=WLAN.STA)
+#wlan.antenna(WLAN.EXT_ANT)
+wlan.connect("BELL094", auth=(WLAN.WPA2, "14FADA65171E"), timeout=30000)
+
+while not wlan.isconnected(): 
+     machine.idle()
+
+print("Connected to Wifi\n")
+client = MQTTClient(' ', 'test.mosquitto.org', port=1883, keepalive=30)
+client.settimeout = settimeout
+client.connect()
+
+while True:
+     print("Sending ON")
+     client.publish(topic="/lights", msg="ON")
+     time.sleep(5)
+     print("Sending OFF")
+     client.publish(topic="/lights", msg="OFF")
+     time.sleep(5)
